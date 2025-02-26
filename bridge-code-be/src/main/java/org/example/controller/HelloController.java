ff7ef47cdef3b9023f978774c57df45f44312255
Leo Kim <imleokkkk@naver.com>
2025-02-26 18:59:51 +0900

    fix : java version changed to 17.0.14

---
+++ bridge-code-be/src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+}

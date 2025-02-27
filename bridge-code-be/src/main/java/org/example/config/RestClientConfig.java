8b557c3666e6809579869db135816c24787c42ed
Leo Kim <imleokkkk@naver.com>
2025-02-28 06:40:04 +0900

    fefw

--- bridge-code-be/src/main/java/org/example/config/RestClientConfig.java
+++ bridge-code-be/src/main/java/org/example/config/RestClientConfig.java
@@ -9,8 +9,8 @@ public class RestClientConfig {
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl("https://c848-211-244-225-164.ngrok-free.app")
-//                .baseUrl("http://ai:5000")
+//                .baseUrl("https://c848-211-244-225-164.ngrok-free.app")
+                .baseUrl("http://ai:5000")
                 .build();
     }
 }

fecc0e7ec71c50dc1ca5c8e542df226152d20aed
Leo Kim <imleokkkk@naver.com>
2025-02-28 03:41:25 +0900

    zz

--- bridge-code-be/src/main/java/org/example/config/CorsConfig.java
+++ bridge-code-be/src/main/java/org/example/config/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 모든 엔드포인트에 CORS 적용
                         .allowedOriginPatterns("*")
-                        .allowedOrigins("*")// 모든 도메인 허용 (보안상 주의)
+                        // 모든 도메인 허용 (보안상 주의)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 지정
                         .allowedHeaders("*")  // 모든 헤더 허용
-                        .allowCredentials(false); // 인증 정보 포함 X
+                        .allowCredentials(true); // 인증 정보 포함 X
             }
         };
     }
